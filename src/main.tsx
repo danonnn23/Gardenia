a4096ce657989f04844864e68cb96a9fde6a011b
danonnn23 <tolochkodan.work@gmail.com>
2026-03-21 21:36:34 +0200

    project setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Home from './pages/Home'
+
+// Створюємо карту маршрутів
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // App буде нашою "оболонкою" з меню
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return(
+        <div className="py-10">
+            <h1 className="text-4xl font-serif text-emerald-900">Наші найкращі букети</h1>
+            <p className="text-gray-600 mt-2">Оберіть свіжість для вашого дому</p>
+        </div>
+    );
+};
+
+export default Home;
